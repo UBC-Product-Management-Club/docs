b1872e7384fdc19df45e635836e1ea6b3d95e341
Kevin Tan <kevin2012tan@gmail.com>
2023-03-12 11:28:41 -0700

    redirect hero page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,60 +1,60 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "UBC Product Management Club",
-  tagline: "",
-  favicon: "img/favicon.ico",
+  title: 'UBC Product Management Club',
+  tagline: '',
+  favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: "http://docs.ubcpm.club/",
+  url: 'http://docs.ubcpm.club/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "UBC-Product-Management-Club", // Usually your GitHub org/user name.
-  projectName: "docs", // Usually your repo name.
+  organizationName: 'UBC-Product-Management-Club', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
   trailingSlash: false,
-  deploymentBranch: "gh-pages",
+  deploymentBranch: 'gh-pages',
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/UBC-Product-Management-Club/docs/tree/main",
+            'https://github.com/UBC-Product-Management-Club/docs/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/UBC-Product-Management-Club/docs/tree/main",
+            'https://github.com/UBC-Product-Management-Club/docs/tree/main',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
@@ -64,25 +64,25 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: "PMC",
+        title: 'PMC',
         // logo: {
         //   alt: "My Site Logo",
         //   src: "img/logo.svg",
         // },
         items: [
           {
-            type: "doc",
-            docId: "intro",
-            position: "left",
-            label: "Welcome",
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Welcome',
           },
-          { to: "/blog", label: "Blog", position: "left" },
+          { to: '/blog', label: 'Blog', position: 'left' },
         ],
       },
       footer: {
-        style: "dark",
+        style: 'dark',
         links: [],
         copyright: `Copyright © ${new Date().getFullYear()} UBC Product Management Club. Built with Docusaurus.`,
       },
@@ -90,6 +90,11 @@ const config = {
       //   theme: lightCodeTheme,
       //   darkTheme: darkCodeTheme,
       // },
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
     }),
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,19 @@
-import React from "react";
-import clsx from "clsx";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import React from 'react';
+import clsx from 'clsx';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import { Redirect } from '@docusaurus/router';
 
-import styles from "./index.module.css";
+import styles from './index.module.css';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className='container'>
+        <h1 className='hero__title'>{siteConfig.title}</h1>
+        <p className='hero__subtitle'>{siteConfig.tagline}</p>
       </div>
     </header>
   );
@@ -21,14 +22,15 @@ function HomepageHeader() {
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
+    // <Layout
+    //   title={`${siteConfig.title}`}
+    //   description='Description will go into a meta tag in <head />'
+    // >
+    //   <HomepageHeader />
+    //   <main>
+    //     <HomepageFeatures />
+    //   </main>
+    // </Layout>
+    <Redirect to='/docs/intro' />
   );
 }
