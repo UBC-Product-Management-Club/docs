ecc60ecf6116a6849a5a4dd649c0cd942890cb07
Kevin Tan <kevin2012tan@gmail.com>
2023-07-16 15:23:01 -0700

    added styling to all text in project to apply inter

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import YourExperience from '@site/src/components/YourExperience';
 
 import styles from './index.module.css';
 
@@ -27,6 +28,7 @@ export default function Home() {
     >
       {/* <HomepageHeader /> */}
       <main>
+        <YourExperience/>
         {/* <HomepageFeatures /> */}
       </main>
     </Layout>
