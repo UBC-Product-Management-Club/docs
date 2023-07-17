75b21fa98bab23488011ad52f769a3b304a53f6c
Minh Au <auduongminh2004@gmail.com>
2023-07-16 23:51:59 -0700

    add see all feature to carousel

--- src/components/Alumni/Alumni.jsx
+++ src/components/Alumni/Alumni.jsx
@@ -4,12 +4,26 @@ import AlumniCarousel from "./AlumniCarousel";
 function Alumni() {
     return (
         <section className="flex flex-col items-center gap-y-6">
-            <div  className="flex flex-col items-center gap-y-4">
-                <h1 className="text-6xl font-medium">Careers - where are our alumni?</h1>
-                <h3 className="text-[1.18rem]">Our programs prepare you for a career as product manager! </h3>
-            </div>
+            <AlumniHeader/>
             <AlumniCarousel/>
+            <SeeAllFeature className="relative bottom-12"/>
         </section>
+    )
+}
+
+function AlumniHeader() {
+    return (
+        <div  className="flex flex-col items-center gap-y-4">
+            <h1 className="text-6xl font-medium">Careers - where are our alumni?</h1>
+            <h3 className="text-[1.18rem]">Our programs prepare you for a career as product manager! </h3>
+        </div>
+    )
+}
+
+function SeeAllFeature({className}) {
+    return (
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className={"w-[15.125rem] h-[2.99663rem] bg-[#FD2A36] rounded-[2.5625rem] flex items-center\
+         justify-center font-inter text-xl font-bold" + " " + className}>See All</a> 
     )
 }
 
