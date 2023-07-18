112cb99058fcb3748cf4ffcd09ee9e63a230c8d4
Minh Au <auduongminh2004@gmail.com>
2023-07-18 11:14:28 -0700

    define custom styles for more consistency

--- src/components/Alumni/Alumni.jsx
+++ src/components/Alumni/Alumni.jsx
@@ -14,8 +14,8 @@ function Alumni() {
 function AlumniHeader() {
     return (
         <div  className="flex flex-col items-center gap-y-4">
-            <h1 className="text-6xl font-medium">Careers - where are our alumni?</h1>
-            <h3 className="text-[1.18rem]">Our programs prepare you for a career as product manager! </h3>
+            <h1 className="text-h1 font-header">Careers - where are our alumni?</h1>
+            <h3 className="text-h3">Our programs prepare you for a career as product manager! </h3>
         </div>
     )
 }
@@ -23,7 +23,7 @@ function AlumniHeader() {
 function SeeAllFeature({className}) {
     return (
         <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className={"w-[15.125rem] h-[2.99663rem] bg-[#FD2A36] rounded-[2.5625rem] flex items-center\
-         justify-center font-inter text-xl font-bold" + " " + className}>See All</a> 
+         justify-center font-inter text-button font-header" + " " + className}>See All</a> 
     )
 }
 
--- src/components/Alumni/AlumniCard.jsx
+++ src/components/Alumni/AlumniCard.jsx
@@ -14,15 +14,15 @@ function AlumniCard({name, title, company, faculty, img}) {
     return (
         <div className="w-[23.76669rem] h-[13.03331rem] rounded-[6.25rem] bg-[#2B3950]
             flex flex-row py-[2.06rem] content-center
-            font-extralight">
+            font-content">
             <img src={img} onError={imgOnError} className="w-[8rem] h-[8rem] rounded-full mt-[0.56rem] ml-[1.69rem] mr-[1.8rem]
                 drop-shadow-[0px_6px_11px_rgba(0, 0, 0, 0.25)]"/>
-            <div className="flex flex-col text-xs gap-y-[1.94rem]">
+            <div className="flex flex-col text-p gap-y-[1.94rem]">
                 <div>
-                    <div className="text-2xl font-bold w-[9.5rem] h-[1.875rem]">{name}</div>
+                    <div className="text-h2 font-header w-[9.5rem] h-[1.875rem]">{name}</div>
                     <div>{title}</div>
                 </div>
-                <div className="font-bold flex flex-row gap-x-[0.5rem]">
+                <div className="font-header flex flex-row gap-x-[0.5rem]">
                     <text className="my-[0.25rem]">@{company}</text>
                     <img src={logoLib[company]}/>
                 </div>
