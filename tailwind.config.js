f42ddcf586d1d8ee15476d14bc3f32169125fcd6
Ayden1456 <89323066+Ayden1456@users.noreply.github.com>
2023-07-20 11:00:51 -0700

    fix config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'inter' : ['Inter', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif'],
+      },
+      fontSize: {
+        'h1': ['3.75rem'],
+        'h2': ['1.5rem'],
+        'h3': ['1.18rem'],
+        'p': ['0.75rem'],
+        'button': ['1.25rem'],
+      },
+      fontWeight: {
+        'header': ['700'],
+        'content': ['200'],
       }
     },
   },
