357569040927307ecfe51d327c8094bc4fd8b981
Kevin Tan <kevin2012tan@gmail.com>
2023-07-25 22:03:29 -0700

    took out unnecessary CSS props

--- src/components/shared/ExpandButton.jsx
+++ src/components/shared/ExpandButton.jsx
@@ -7,11 +7,11 @@ function ExpandButton({ text }) {
       href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
       className={
         'w-[15.125rem] h-[2.99663rem] bg-[#FD2A36] rounded-[2.5625rem] flex items-center\
-    justify-center text-button font-header relative bottom-12 border-0 cursor-pointer\
-    px-8 font-bold text-md primary transition-colors hover:glow-neon-red no-underline'
+    justify-center text-button relative bottom-12 border-0 cursor-pointer\
+    font-bold hover:glow-neon-red no-underline'
       }
     >
-      <p className='text-white group-hover:bg-red-600 group-hover:text-white group-hover:shadow-md group-hover:ring-4 group-hover:ring-red-600'>
+      <p className='text-white group-hover:bg-red-600 group-hover:shadow-md group-hover:ring-4 group-hover:ring-red-600'>
         {text}
       </p>
     </a>
