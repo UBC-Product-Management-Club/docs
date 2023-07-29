b88a4f952678f29728f2958efb3326cb24120580
Ayden1456 <89323066+Ayden1456@users.noreply.github.com>
2023-07-29 09:22:15 -0700

    finish buttons and text

--- src/components/HeroContainer/HeroContainer.jsx
+++ src/components/HeroContainer/HeroContainer.jsx
@@ -1,18 +1,31 @@
 import React from "react";
+import HeroFolder from "@site/resources/Images/pmcImage.jpeg";
 // make sure that the first letter is capitalized
 export default function HeroContainer() { // put things in here if you want to pass info as props 
     return ( // here are the arguments that we pass into the component
       <>
-      
-      <div className="">
-        <div className="text-white text-4xl font-bold m-5">
-          BEGIN YOUR PRODUCT MANAGEMENT MASTERY.
+      <div className="ml-0 grid grid-cols-2 mt-7 p-5 h-[25rem] w-full px-14">
+        <div className="text-white font-bold ml-7" style={{fontSize: '45px'}}>
+          <div className="">
+            BEGIN YOUR <br />
+            PRODUCT <br />
+            MANAGEMENT
+            <br />
+            MASTERY.
+          </div>
+          <div className="mt-5 ml-1">
+            <button className="px-6 py-3 text-xl text-white font-semibold rounded-full border border-red-600 bg-red-500 hover:text-white hover:bg-red-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2">Start Here</button>
+            <button className="ml-3 px-6 py-3 text-xl text-red-600 font-semibold rounded-full border border-red-600 hover:text-white hover:bg-red-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2">Explore Our Programs</button>
+          </div>
+          
         </div>
-        <div class="ml-3">
-          <button className="px-4 m-3 py-1 text-sm text-white font-semibold rounded-full border border-red-600 bg-red-500 hover:text-white hover:bg-red-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2">Start Here</button>
-          <button className="px-4 m-3 py-1 text-sm text-red-600 font-semibold rounded-full border border-red-600 hover:text-white hover:bg-red-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2">Explore Our Programs</button>
+        
+       
+        <div className="rounded-md flex flex-1 items-center justify-center">
+          <img src={HeroFolder} className="rounded-lg" style={{width: '600', height: '400px'}} alt="" />
+          
         </div>
-        <img src="@site/resources/images/internshiphero.jpeg" alt="" />
+        
       </div>
 
       </> //what is this jsx wrap for??
