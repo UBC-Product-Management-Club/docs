eefa7c1149df14221e20a2e6100a9d14334a22f6
Kevin Tan <kevin2012tan@gmail.com>
2023-07-31 19:27:38 -0700

    added two other experts

--- src/components/Experts/ExpertCard.jsx
+++ src/components/Experts/ExpertCard.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import Rachit from '../../../static/img/experts/rachit.png';
-import LinkedIn from '../../../static/img/experts/icons/linkedin.png';
-import Info from '../../../static/img/experts/icons/info.png';
+import LinkedInImg from '../../../static/img/experts/icons/linkedin.png';
+import InfoImg from '../../../static/img/experts/icons/info.png';
 
-function ExpertCard() {
+function ExpertCard({ name, content, LinkedIn, img }) {
   return (
-    <div className='w-[60.375rem] h-[10.25rem] rounded-[1.20275rem] bg-[#2B3950] flex relative'>
+    <div className='w-[60.375rem] h-[10.25rem] rounded-[1.20275rem] bg-[#2B3950] flex relative mt-[1.69rem]'>
       <div className='absolute left-[-1rem] flex'>
-        <img src={Rachit} alt='' />
+        <img src={img} alt='' />
         <div className=' ml-[2rem] mt-[0.94rem]'>
           <h3 className='text-h2 font-[700]'>
-            Rachit: Conducting User Interviews: Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit
+            {name}: {content}
           </h3>
           {/* Icons */}
           <div className='flex gap-[1.12rem] mt-[1.44rem]'>
             <a href=''>
-              <img src={LinkedIn} alt='' />
+              <img src={LinkedInImg} alt='' />
             </a>
             <a href=''>
-              <img src={Info} alt='' />
+              <img src={InfoImg} alt='' />
             </a>
           </div>
         </div>
