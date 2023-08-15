e2bc950d46a19782161432757c9add57c56020f9
Kevin Tan <kevin2012tan@gmail.com>
2023-08-14 19:59:31 -0700

    removed prev button

--- src/components/Alumni/Alumni.jsx
+++ src/components/Alumni/Alumni.jsx
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import AlumniCarousel from './AlumniCarousel';
-import ExpandButton from '../shared/ExpandButton';
-
-function Alumni() {
-  return (
-    <section className='my-[6.87rem] flex flex-col items-center gap-y-6'>
-      <AlumniHeader />
-      <AlumniCarousel />
-      <ExpandButton text={'See All'} />
-      {/* <SeeAllFeature className='relative bottom-12' /> */}
-    </section>
-  );
-=======
 import React, {useState} from "react";
 import AlumniCarousel from "./AlumniCarousel";
 import ExpandButton from "../shared/ExpandButton";
@@ -25,7 +10,6 @@ function Alumni() {
             <ExpandButton text={"See All"} className={"relative bottom-12"} button_type={"primary"}/>
         </section>
     )
->>>>>>> main
 }
 
 function AlumniHeader() {
@@ -38,24 +22,5 @@ function AlumniHeader() {
     </div>
   );
 }
-
-<<<<<<< HEAD
-function SeeAllFeature({ className }) {
-  return (
-    <a
-      href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-      className={
-        'w-[15.125rem] h-[2.99663rem] bg-[#FD2A36] rounded-[2.5625rem] flex items-center\
-         justify-center font-inter text-button font-header' +
-        ' ' +
-        className
-      }
-    >
-      See All
-    </a>
-  );
-}
-=======
->>>>>>> main
 
 export default Alumni;
