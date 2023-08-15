2ac4209b2437323b987010e95e36ad7cf4cff8d7
Kevin Tan <kevin2012tan@gmail.com>
2023-08-14 20:07:08 -0700

    linkedin url prop

--- src/components/Experts/ExpertCard.jsx
+++ src/components/Experts/ExpertCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import LinkedInImg from '../../../static/img/experts/icons/linkedin.png';
 import InfoImg from '../../../static/img/experts/icons/info.png';
 
-function ExpertCard({ name, content, LinkedIn, img }) {
+function ExpertCard({ name, content, linkedInUrl, img }) {
   return (
     <div className='w-[60.375rem] h-[10.25rem] rounded-[1.20275rem] bg-[#2B3950] flex relative mt-[1.69rem]'>
       <div className='absolute left-[-1rem] flex'>
@@ -13,12 +13,10 @@ function ExpertCard({ name, content, LinkedIn, img }) {
           </h3>
           {/* Icons */}
           <div className='flex gap-[1.12rem] mt-[1.44rem]'>
-            <a href=''>
-              <img src={LinkedInImg} alt='' />
+            <a href={linkedInUrl}>
+              <img src={LinkedInImg} alt='linkedIn url' />
             </a>
-            <a href=''>
-              <img src={InfoImg} alt='' />
-            </a>
+            <img src={InfoImg} alt='more information' />
           </div>
         </div>
       </div>
