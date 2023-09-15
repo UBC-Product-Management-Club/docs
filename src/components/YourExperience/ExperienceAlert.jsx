dc3fe6084a245704ad5eabd4cd31d480492b751b
Kevin Tan <kevin2012tan@gmail.com>
2023-09-14 20:40:17 -0700

    mobile friendly your experience

--- src/components/YourExperience/ExperienceAlert.jsx
+++ src/components/YourExperience/ExperienceAlert.jsx
@@ -6,7 +6,7 @@ export default function ExperienceAlert({ text }) {
   };
   return (
     <h3
-      className={`rounded-[2.5625rem] bg-[#FD2A36] text-white p-[1.4rem] absolute top-[-1rem] duration-300 delay-300`}
+      className={`text-[0.75rem] md:text-[1rem] rounded-[2.5625rem] bg-[#FD2A36] text-white p-[1.4rem] absolute top-[-1rem] duration-300 delay-300`}
       style={{ right: `${calculateRightPosition()}px` }}
     >
       {text}
--- src/components/YourExperience/YourExperience.jsx
+++ src/components/YourExperience/YourExperience.jsx
@@ -9,20 +9,20 @@ import ExpandButton from '../shared/ExpandButton';
 export default function YourExperience() {
   return (
     <div className='my-28 h-40vh flex flex-col items-center'>
-      <h1 className='text-h1 font-extrabold mb-4'>
+      <h1 className='text-[1.11544rem] md:text-h1 md:font-extrabold md:mb-4 md:text-center md:max-w-[50rem] lg:max-w-full'>
         What can your experience look like?
       </h1>
-      <h2 className='text-h3 font-extrabold'>
+      <h2 className='text-[0.75rem] md:text-h3 md:font-extrabold '>
         You can use this as your guide to become a better Product Manager
       </h2>
 
       {/* Wrapper for experiences*/}
-      <div className='sm:w-[72rem] sm:h-[25rem] h-[64rem] rounded-[5.4375rem] bg-[#35394A] my-8 shadow-lg flex sm:flex-row flex-col items-center justify-around px-[4rem] text-black py-10'>
+      <div className='md:w-[72rem] md:h-[25rem] h-[64rem] rounded-[5.4375rem] bg-[#35394A] my-8 shadow-lg flex md:flex-row flex-col items-center justify-around px-[4rem] text-black py-10'>
         <ExperienceCard
           imageSource={LightBulbImg}
           subtitle={'Conducting User Interviews'}
           text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}
-          color={"cyan"}
+          color={'cyan'}
         >
           <ExperienceAlert text={'New!'} />
         </ExperienceCard>
@@ -42,7 +42,11 @@ export default function YourExperience() {
         </ExperienceCard>
       </div>
 
-      <ExpandButton text={'Learn More!'} className={"relative bottom-[3rem]"} button_type={"primary"}/>
+      <ExpandButton
+        text={'Learn More!'}
+        className={'relative bottom-[3rem]'}
+        button_type={'primary'}
+      />
     </div>
   );
 }
